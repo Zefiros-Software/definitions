f28fdeaca5b6730f5424921a5237c97f99ac618b
Paul Visscher <p.e.visscher@zefiros.io>
2019-07-28 15:51:50 +0200

    chore: add unix support

--- boost/extract/extract.ts
+++ boost/extract/extract.ts
@@ -2,19 +2,19 @@ function isWindows() {
     return platform.os === "win32"
 }
 
+const b2 = isWindows() ? 'b2.exe' : './b2'
+const bootstrap = isWindows() ? 'bootstrap.bat' : 'bash bootstrap.sh'
+const bcp = isWindows() ? 'dist/bin/bcp.exe' : 'dist/bin/bcp'
+
+
 export async function extract() {
     if (version.settings.modules && version.settings.modules.length > 0) {
-        await shell.exec(`.\\dist\\bin\\bcp.exe ${version.settings.modules.join(' ')} ${version.target}`)
+        await shell.exec(`${bcp} ${version.settings.modules.join(' ')} ${version.target}`)
     }
 }
 
 export async function checkout() {
     await git.checkout(version.hash!)
-
-    const b2 = isWindows() ? 'b2.exe' : 'b2'
-    const bootstrap = isWindows() ? 'bootstrap.bat' : './bootstrap.sh'
-    const bcp = isWindows() ? 'dist/bin/bcp.exe' : 'dist/bin/bcp'
-    
     if (!fs.exists(b2)) {        
         await shell.exec(bootstrap)
     }
