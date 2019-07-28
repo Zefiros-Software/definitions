4139ea8f85ad849d9072ab267551fbec42f1fbb8
Paul Visscher <p.e.visscher@zefiros.io>
2019-07-28 15:40:49 +0200

    chore: add unix support

--- boost/extract/extract.ts
+++ boost/extract/extract.ts
@@ -1,3 +1,7 @@
+function isWindows() {
+    return platform.os === "win32"
+}
+
 export async function extract() {
     if (version.settings.modules && version.settings.modules.length > 0) {
         await shell.exec(`.\\dist\\bin\\bcp.exe ${version.settings.modules.join(' ')} ${version.target}`)
@@ -6,20 +10,20 @@ export async function extract() {
 
 export async function checkout() {
     await git.checkout(version.hash!)
+
+    const b2 = isWindows() ? 'b2.exe' : 'b2'
+    const bootstrap = isWindows() ? 'bootstrap.bat' : './bootstrap.sh'
+    const bcp = isWindows() ? 'dist/bin/bcp.exe' : 'dist/bin/bcp'
     
-    if (!fs.exists('b2.exe')) {        
-        await shell.exec('.\\bootstrap.bat')
+    if (!fs.exists(b2)) {        
+        await shell.exec(bootstrap)
     }
     
-    if (!fs.exists('dist/bin/bcp.exe')) {       
-        await shell.exec('.\\b2.exe -j 12 tools\\bcp')
+    if (!fs.exists(bcp)) {       
+        await shell.exec(`${b2} -j 12 tools/bcp`)
     }
-    
-    if (!fs.exists('dist/bin/bcp.exe')) {       
-        await shell.exec('.\\b2.exe -j 12 tools\\bcp')
-    }
-    
+
     if (!fs.exists('boost')) {       
-        await shell.exec('.\\b2.exe headers -j 12')
+        await shell.exec(`${b2} headers -j 12`)
     }
 }
